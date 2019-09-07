cb596e6c770b7d165d3567f8e4a643ce549d3a6e
Simen Bekkhus <sbekkhus91@gmail.com>
2019-09-07 06:47:16 +0200

    chore: remove unused jest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testPathIgnorePatterns: ['/examples/', '/node_modules/', '/__eslint__/'],
-  testMatch: [
-    '<rootDir>/src/**/__tests__/**/*.js',
-    '<rootDir>/integrationTests/*.test.js',
-    '<rootDir>/integrationTests/**/*.test.js',
-  ],
-};
